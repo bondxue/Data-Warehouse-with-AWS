82770378e356990aa9b2a7667051e6850e80a14e
mengheng <menghengxue@gmail.com>
2019-07-06 21:29:08 -0400

    update sql queries

--- sql_queries.py
+++ sql_queries.py
@@ -7,35 +7,110 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 staging_events_table_create= ("""
+CREATE TABLE staging_events(
+    event_id INT IDENTITY(0,1),
+    artist_name VARCHAR(255),
+    auth VARCHAR(50),
+    user_first_name VARCHAR(255),
+    user_gender  VARCHAR(1),
+    item_in_session	INTEGER,
+    user_last_name VARCHAR(255),
+    song_length	DOUBLE PRECISION, 
+    user_level VARCHAR(50),
+    location VARCHAR(255),	
+    method VARCHAR(25),
+    page VARCHAR(35),	
+    registration VARCHAR(50),	
+    session_id	BIGINT,
+    song_title VARCHAR(255),
+    status INTEGER,  -- SHOULD BE ALWAYS A NUMBER, HTTP STATUS	
+    ts VARCHAR(50),
+    user_agent TEXT,	
+    user_id VARCHAR(100),
+    PRIMARY KEY (event_id))
+)
 """)
 
 staging_songs_table_create = ("""
+CREATE TABLE staging_songs(
+    song_id VARCHAR(100),
+    num_songs INTEGER,
+    artist_id VARCHAR(100),
+    artist_latitude DOUBLE PRECISION,
+    artist_longitude DOUBLE PRECISION,
+    artist_location VARCHAR(255),
+    artist_name VARCHAR(255),
+    title VARCHAR(255),
+    duration DOUBLE PRECISION,
+    year INTEGER,
+    PRIMARY KEY (song_id))
 """)
 
 songplay_table_create = ("""
+CREATE TABLE songplays(
+    songplay_id INT IDENTITY(0,1),
+    start_time TIMESTAMP REFERENCES time(start_time),
+    user_id VARCHAR(50) REFERENCES users(user_id),
+    level VARCHAR(50),
+    song_id VARCHAR(100) REFERENCES songs(song_id),
+    artist_id VARCHAR(100) REFERENCES artists(artist_id),
+    session_id BIGINT,
+    location VARCHAR(255),
+    user_agent TEXT,
+    PRIMARY KEY (songplay_id))
 """)
 
 user_table_create = ("""
+CREATE TABLE users(
+    user_id VARCHAR,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    gender VARCHAR(1),
+    level VARCHAR(50),
+    PRIMARY KEY (user_id))
 """)
 
 song_table_create = ("""
+CREATE TABLE songs(
+    song_id VARCHAR(100),
+    title VARCHAR(255),
+    artist_id VARCHAR(100) NOT NULL,
+    year INTEGER,
+    duration DOUBLE PRECISION,
+    PRIMARY KEY (song_id))
 """)
 
 artist_table_create = ("""
+CREATE TABLE artists(
+    artist_id VARCHAR(100),
+    name VARCHAR(255),
+    location VARCHAR(255),
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION,
+    PRIMARY KEY (artist_id))
 """)
 
 time_table_create = ("""
+CREATE TABLE time(
+    start_time TIMESTAMP,
+    hour INTEGER,
+    day INTEGER,
+    week INTEGER,
+    month INTEGER,
+    year INTEGER,
+    weekday INTEGER,
+    PRIMARY KEY (start_time))
 """)
 
 # STAGING TABLES
